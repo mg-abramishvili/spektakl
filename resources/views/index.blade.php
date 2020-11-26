4570ef4b7f9b1e9e0c3ffc7eb68f96875261764d
mg-abramishvili <mg@abramishvili.net>
2020-11-26 16:43:10 +0500

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     @if (\App\Models\Ticket::all()->where('date', \Carbon\Carbon::today()->format('Y-m-d'))->count() <= 75)
     <div class="wrapper" style="background-image: url(/img/bg01.png);">
         <a href="/front-shows" class="btn-standard" style="position: absolute; bottom: 100px; left: 0; right: 0; margin: 0 auto; padding: 30px; width: 300px;">Выбрать сеанс</a>
+        <div class="qr" style="position: absolute; bottom: 50px; right: 50px; width: 150px">
+    <img src="/img/qr.jpg" style="width:100%;">
+</div>
         </div>
     @else
     <div class="wrapper" style="">
@@ -16,8 +19,12 @@
 <br><br>Время сеансов: <br>10:00, 12:00, 14:00 и 16:00
 
 <br>Ежедневно</p>
+<div class="qr" style="position: absolute; bottom: 50px; right: 50px; width: 150px">
+    <img src="/img/qr.jpg" style="width:100%;">
+</div>
         </div>
     @endif
+
 
 
 @endsection
